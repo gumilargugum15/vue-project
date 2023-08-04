cd06073c32e3e87cee0977dcbbbb20dbca32b39a
kagoem <129960411+kagoem@users.noreply.github.com>
2023-08-04 07:12:59 +0700

    update form login

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,13 +1,22 @@
 <template>
-
-  <form @submit.prevent="login">
-  <h2>Log in</h2>
+  <!-- <form @submit.prevent="login">
+    <h2>Log in</h2>
     <InputText type="text" v-model="value" />
-    <input type="email"    placeholder="Email"    required v-model="email">
+    <input type="email" placeholder="Email" required v-model="email">
     <input type="password" placeholder="Password" required v-model="password">
-    <button type="button" class="btn btn-primary">Login</button>
-  </form>
-  
+    <button>Login</button>
+  </form> -->
+  <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="email">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" v-model="password">
+  </div>
+  <button type="button" class="btn btn-primary btn-sm" @click="login">Login</button>
+</form>
 </template>
 
 <script>
@@ -16,22 +25,22 @@ import { auth } from '../firebase/init.js'
 import { ref } from 'vue';
 const value = ref(null);
 export default {
-  components:{
+  components: {
   },
   data() {
     return {
-      email:    '',
+      email: '',
       password: ''
     }
   },
   methods: {
     login() {
       // login user
-      signInWithEmailAndPassword(auth,this.email,this.password)
-      .then(() => {
-        // emit event for member area
-        this.$emit('loggedIn')
-      })
+      signInWithEmailAndPassword(auth, this.email, this.password)
+        .then(() => {
+          // emit event for member area
+          this.$emit('loggedIn')
+        })
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
-import './assets/main.css'
-import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js'
 const app = createApp(App)
+app.use(bootstrap)
 app.use(router)
 app.mount('#app')
-import "bootstrap/dist/js/bootstrap.js"
